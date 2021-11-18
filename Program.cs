bfc3cc43fdbe4ec873817665cde6e5a6a7bff306
AntonC9018 <anton.curm@gmail.com>
2021-11-18 19:32:31 +0200

    Neat

--- Program.cs
+++ Program.cs
@@ -1,120 +1,174 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using static System.Diagnostics.Debug;
 
 namespace UnsafeMessagePipe
 {
-    enum MessageType { A, B }
-    struct MessageA
+    public interface ISubscriberWrapper
     {
-        public int a;
-        public int b;
+        void Execute(ReadOnlySpan<byte> bytes);
+        int GetSize();
     }
-    struct MessageB
+    public interface ISubscriber<T> : ISubscriberWrapper where T : unmanaged 
     {
-        public int a;
-        public int b;
+        void Execute(in T data);
     }
-    struct MessageWithPtr
+    public abstract class Subscriber<T> : ISubscriber<T> where T : unmanaged
     {
-        public int a;
+        public abstract void Execute(in T data);
+
+        // These have to either come from a base class or be generated via the code generator
+        void ISubscriberWrapper.Execute(ReadOnlySpan<byte> bytes)
+        {
+            Execute(in MemoryMarshal.Cast<byte, T>(bytes)[0]);
+        }
+        public int GetSize()
+        {
+            return Marshal.SizeOf<T>();
+        }
     }
 
-    struct Message<MessageBodyType> where MessageBodyType : struct
+    public struct MessageIdentifier<T>
     {
-        public MessageType messageType;
-        public MessageBodyType messageBody;
-        public static int GetSize() => Marshal.SizeOf<Message<MessageBodyType>>();
+        public int id;
+        public MessageIdentifier(int id)
+        {
+            this.id = id;
+        }
+    }
+
+    public class Registry
+    {
+        private int _currentId;
+        public int NextId() { return _currentId++; }
+        public int Count => _currentId;
+    }
+
+    public unsafe class UnsafeMessagePipe
+    {
+        private Memory<byte> _rawData;
+        private int _currentIndex;
+
+        public UnsafeMessagePipe(int maxSize)
+        {
+            _rawData = GC.AllocateArray<byte>(maxSize, false);
+            _currentIndex = 0;
+        }
+
+        public void AddMessage<T>(MessageIdentifier<T> messageType, in T message) where T : unmanaged
+        {
+            using (var handle = _rawData.Pin())
+            {
+                Assert(_currentIndex + 4 + Marshal.SizeOf<T>() <= _rawData.Length);
+
+                *(int*)((byte*) handle.Pointer + _currentIndex) = messageType.id;
+                _currentIndex += 4;
+
+                *(T*)((byte*) handle.Pointer + _currentIndex) = message;
+                _currentIndex += Marshal.SizeOf<T>();
+            }
+        }
+
+        public void Reset() { _currentIndex = 0; }
+        public bool IsEmpty => _currentIndex == 0;
+
+        public ref T GetMessageAt<T>(MessageIdentifier<T> messageType, int index) where T : unmanaged
+        {
+            using (var handle = _rawData.Pin())
+            {
+                Assert(index < _rawData.Length);
+                Assert(*(int*)((byte*) handle.Pointer + index) == messageType.id);
+                return ref Unsafe.AsRef<T>((T*)((byte*) handle.Pointer + index + 4));
+            }
+        }
+
+        public void Flush(ReadOnlySpan<ISubscriberWrapper> eventHandlers)
+        {
+            using (var handle = _rawData.Pin())
+            {
+                int offset = 0;
+
+                while (offset < _currentIndex)
+                {
+                    int rawMessageType = *(int*)((byte*) handle.Pointer + offset);
+                    Assert(rawMessageType < eventHandlers.Length);
+                    offset += 4; // the int
+                    int size = eventHandlers[rawMessageType].GetSize();
+                    var span = new Span<byte>((byte*) handle.Pointer + offset, size);
+                    eventHandlers[rawMessageType].Execute(span);
+                    offset += size;
+                }
+
+                _currentIndex = 0;
+            }
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct DataA
+    {
+        public int a;
+        public int b;
+    }
+    public class DataA_PlaybackSubscriber : Subscriber<DataA>
+    {
+        public override void Execute(in DataA data)
+        {
+            Console.WriteLine("Data A playing back");
+            Console.WriteLine("a:" + data.a);
+            Console.WriteLine("b:" + data.b);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct DataB
+    {
+        public int a;
+        public int b;
+    }
+    public class DataB_PlaybackSubscriber : Subscriber<DataB>
+    {
+        public override void Execute(in DataB data)
+        {
+            Console.WriteLine("Data B playing back");
+            Console.WriteLine("a:" + data.a);
+            Console.WriteLine("b:" + data.b);
+        }
     }
 
     class Program
     {
-        static int GetMessageSize(MessageType m)
-        {
-            return m switch
-            {
-                MessageType.A => Marshal.SizeOf<MessageA>(),
-                MessageType.B => Marshal.SizeOf<MessageB>(),
-                _ => throw new Exception("Impossible")
-            };
-        }
-
-        unsafe void Set<T>(ref byte* memory, in T message, MessageType messageType) where T : unmanaged
-        {
-            *(int*) memory = (int) messageType;
-            memory += sizeof(int);
-            *(T*) memory = message;
-            memory += Marshal.SizeOf<T>();
-        } 
-        
         static void Main(string[] args)
         {
-            unsafe
-            {
-                byte[] array = GC.AllocateArray<byte>(32, pinned: true);
-                {
-                    int offset = 0;
-                    byte* basePointer = (byte*) Marshal.UnsafeAddrOfPinnedArrayElement(array, 0);
-                    Span<int> i = new Span<int>(basePointer, 32 / 4);
-                    *(int*) (basePointer + offset) = (int) MessageType.A; // 4
-                    offset += 4;
-                    int asize = GetMessageSize(MessageType.A); // 8
-                    Assert(asize == 8);
-                    MessageA* aptr = (MessageA*) (basePointer + offset);
-                    aptr->a = 1;
-                    aptr->b = 2;
-                    offset += asize;
+            Registry reg = new Registry();
 
-                    *(int*)(basePointer + offset) = (int) MessageType.B; // 4
-                    offset += 4;
-                    int bsize = GetMessageSize(MessageType.B); // 8
-                    Assert(bsize == 8);
-                    MessageA* bptr = (MessageA*) (basePointer + offset);
-                    bptr->a = 3;
-                    bptr->b = 4;
-                    offset += bsize;
-                }
-                
-                {
-                    int offset;
-                    byte* basePointer = (byte*) Marshal.UnsafeAddrOfPinnedArrayElement(array, 0);
+            MessageIdentifier<DataA> a_index;
+            a_index.id = reg.NextId();
+            MessageIdentifier<DataB> b_index;
+            b_index.id = reg.NextId();
+            
+            DataA data1;
+            data1.a = 1;
+            data1.b = 2;
 
-                    for (offset = 0; offset < array.Length;)
-                    {
-                        int rawMessageType = * (int*) ((byte*) basePointer + offset);
-                        if (rawMessageType == -1)
-                            break;
-                        int size = GetMessageSize((MessageType) rawMessageType);
-                        offset += 4; // the int
+            DataB data2;
+            data2.a = 3;
+            data2.b = 4;
 
-                        switch ((MessageType) rawMessageType)
-                        {
-                            case MessageType.A:
-                            {
-                                MessageA* a = (MessageA*) ((byte*) basePointer + offset);
-                                MessageA acopy = *a;
-                                Console.WriteLine(acopy.a);
-                                Console.WriteLine(acopy.b);
-                                break;
-                            }
-                            
-                            case MessageType.B:
-                            {
-                                MessageB* b = (MessageB*) ((byte*) basePointer + offset);
-                                MessageB bcopy = *b;
-                                Console.WriteLine(bcopy.a);
-                                Console.WriteLine(bcopy.b);
-                                break;
-                            }
+            // This gets the final size. It will never grow.
+            // TODO: maybe make it a circular buffer.
+            var pipe = new UnsafeMessagePipe(64 * 1024);
+            pipe.AddMessage(a_index, data1);
+            pipe.AddMessage(b_index, data2);
+            pipe.AddMessage(a_index, data1);
 
-                            default: Assert(false, "Should never happen"); break;
-                        }
+            var subs = new ISubscriberWrapper[reg.Count];
+            subs[a_index.id] = new DataA_PlaybackSubscriber();
+            subs[b_index.id] = new DataB_PlaybackSubscriber();
 
-                        offset += size;
-                    }
-                }
-            }
+            pipe.Flush(subs);
         }
     }
 }
